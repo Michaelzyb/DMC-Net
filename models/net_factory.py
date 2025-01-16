4ce786fbb5cb8a16d1d870acf8001b869955b9be
Michaelzyb <1287293308@qq.com>
2025-01-16 17:43:12 +0800

    net_factory.py

--- models/net_factory.py
+++ models/net_factory.py
@@ -6,8 +6,8 @@ from models.total_supvised.u_net import *
 
 def get_model(net_type="uaps", in_chns=3, class_num=4):
     # total supervised model
-    if net_type == 'hdrnet':
-        net = HDR_Net(in_chns, class_num)
+    if net_type == 'dmcnet':
+        net = DMC_Net(in_chns, class_num)
     elif net_type == 'u_net':
         net = UNet(in_chns=in_chns, class_num=class_num)
     elif net_type == 'edrnet':
@@ -18,8 +18,6 @@ def get_model(net_type="uaps", in_chns=3, class_num=4):
         net = DeepLabV3(in_chns, class_num)
     elif net_type == 'seg_net':
         net = SegNet(in_chns, class_num)
-    elif net_type == 'swin_unet':
-        net = SwinTransformerSys(in_chans=in_chns, num_classes=class_num, img_size=224)
     elif net_type == 'seg_former':
         net = segformer_b0(progress=False, num_classes=class_num)
     elif net_type == 'pga_net':
@@ -28,5 +26,4 @@ def get_model(net_type="uaps", in_chns=3, class_num=4):
         net = TopFormer(class_num)
     else:
         AttributeError('Please input correct model name!')
-
     return net
